b072af2bb2e3fcb610c7799c1b47d4228d680e90
hominhIsStupid <ngominh04112007@gmail.com>
2026-07-26 00:01:37 +0700

    moved some stuff
    Co-Authored-By: quangminhaaaaa7-eng <quangminhaaaaa7@gmail.com>
    Co-Authored-By: minhdungnguyen24042007-spec <minhdungnguyen24042007@gmail.com>
    Co-Authored-By: cosmoofspace2007-glitch <cosmoofspace2007@gmail.com>

--- forceScrape.js
+++
@@ -1,8 +0,0 @@
-const { runResearchScraper } = require('./src/backend/services/researchScraperService');
-
-async function forceScrape() {
-  await runResearchScraper();
-  setTimeout(() => process.exit(0), 2000);
-}
-
-forceScrape();
